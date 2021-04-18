070d225d1dd2ceb9e92cc418d78e23a01df848ee
Arthur Volokhin <artur.volokhin@mail.ru>
2021-04-18 16:14:11 +0300

    refactor: data.js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,27 +1,11 @@
-export const data = [
+let cardData = [
     {
         id: "#todo",
         title: "Todo",
         counter: 0,
         btnDeleteCard: "kanban__column-delete-cards",
         btnCreateCard: "kanban__column-add-elements",
-        todos: [
-            {
-                id: 1,
-                title: "Создать кнопку",
-                comment: "Очень большую большую кнопку!)",
-                date: "21:30",
-                autor: "Artur",
-            },
-
-            {
-                id: 2,
-                title: "Создать кнопку",
-                comment: "Очень большую большую кнопку!)",
-                date: "21:30",
-                autor: "Artur",
-            },
-        ],
+        todos: [],
     },
 
     {
@@ -30,15 +14,7 @@ export const data = [
         counter: 0,
         columnClass: "kanban__column kanban__column--progress",
         btnDeleteCard: "kanban__column-delete-cards",
-        todos: [
-            {
-                id: 3,
-                title: "Создать модалку",
-                comment: "Очень большую большую модалку!)",
-                date: "20:17",
-                autor: "Anton",
-            },
-        ],
+        todos: [],
     },
 
     {
@@ -47,16 +23,7 @@ export const data = [
         counter: 0,
         columnClass: "kanban__column kanban__column--done",
         btnDeleteCard: "kanban__column-delete-cards",
-        todos: [
-            {
-                id: 4,
-                title: "Сделать зарядку",
-                comment: "Ну хотя бы чуть-чуть))!)",
-                date: "7:30",
-                autor: "Artur",
-            },
-        ],
+        todos: [],
     },
+    
 ];
-
-// localStorage.setItem('todos', JSON.stringify(data));
