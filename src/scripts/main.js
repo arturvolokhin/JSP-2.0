7d7f9876d33f3e3795f3dc753f6fd71ed470bf8b
Arthur Volokhin <artur.volokhin@mail.ru>
2021-04-18 17:24:20 +0300

    feat: Add a condition to the event listener to add a card

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,4 +10,11 @@ let column = new Column(getElementInLocalStorage('todos'));
 
 document.addEventListener('click', (e) => {
     
+    if (e.target.classList.contains('kanban__modal-btn--add')){
+        let modal = document.querySelector(".kanban__modal");
+        let title = modal.querySelector("input").value;
+        let comment = modal.querySelector("textarea").value;
+        let author = modal.querySelector(".kanban__modal-list").value;
+        column.addNewCard(title, comment, author);
+    }
 });
