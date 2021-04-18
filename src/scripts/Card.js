c1bfcc737ad8be1061ed2ec272bb82ddcd77137b
Arthur Volokhin <artur.volokhin@mail.ru>
2021-04-18 17:13:02 +0300

    feat: Add card-constructor in Card.js

---
+++ src/scripts/Card.js
@@ -0,0 +1,7 @@
+export function CreateNewCard(titleText, cardTextInput, cardAuthor, cardDate, cardId) {
+    this.title = titleText; 
+    this.comment = cardTextInput; 
+    this.date = cardDate;
+    this.author = cardAuthor;
+    this.id = cardId;
+}
