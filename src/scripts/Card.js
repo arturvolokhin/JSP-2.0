3e81a09a7485d323c73657184d7163fc89f660a4
Arthur Volokhin <67961676+arturvolokhin@users.noreply.github.com>
2021-04-19 19:34:29 +0300

    Delete src directory

--- src/scripts/Card.js
+++
@@ -1,7 +0,0 @@
-export function CreateNewCard(titleText, cardTextInput, cardAuthor, cardDate, cardId) {
-    this.title = titleText; 
-    this.comment = cardTextInput; 
-    this.date = cardDate;
-    this.author = cardAuthor;
-    this.id = cardId;
-}
